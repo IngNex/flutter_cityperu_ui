df7297ff6cfbd372b8d516598d29b795b2f9d352
IngNex <ingnexmaicol14@gmail.com>
2023-07-30 00:07:12 -0500

    add models and data memory

---
+++ lib/data/memory_cityperu.dart
@@ -0,0 +1,119 @@
+import 'package:flutter_cityperu_ui/domain/models/cityperu_models.dart';
+
+final cityPeru = [
+  CityPeru(
+    imageBack: 'assets/machu-picchu.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/arequipa-peru-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/ayacucho-peru-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/cajamarca-peru-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/callao-peru.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/chachapoyas-peru.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/chiclayo-peru-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/cusco-peru-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/huancayo-peru.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/ica-peru.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/iquitos-peru-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/lima-peru-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/lineas-de-nazca-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/montana-ausangate.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/montana-yerupaja-grande.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/museo-senor-de-sipan-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/pacaya-samiria.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/parque-nacional-huascaran.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/montana-vinicunca.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/piura-peru.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/reserva-nacional-de-paracas-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/taquile-peru.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+  CityPeru(
+    imageBack: 'assets/trujillo-peru-1.jpg',
+    name: 'machu-picchu',
+    city: 'Cuzco',
+  ),
+];
---
+++ lib/domain/models/cityperu_models.dart
@@ -0,0 +1,10 @@
+class CityPeru {
+  final String imageBack;
+  final String name;
+  final String city;
+  CityPeru({
+    required this.imageBack,
+    required this.name,
+    required this.city,
+  });
+}
